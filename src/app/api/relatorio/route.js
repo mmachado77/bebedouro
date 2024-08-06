d9ced98c637bff43555e0ccd921396bd185c6db6
Matheus Machado <matheusmachado77@gmail.com>
2024-08-06 16:44:33 -0300

    criacao API relatorio

---
+++ src/app/api/relatorio/route.js
@@ -0,0 +1,10 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+export async function GET(request) {
+
+  const bebeus = await prisma.bebeu.findMany();
+   
+    return Response.json( bebeus )
+  }
